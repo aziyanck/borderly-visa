f726f3e42da324361d4766b0bcb9407688605c76
SerAbinU1 <abinyt1234@gmail.com>
2025-07-15 09:56:28 +0000

    Remove pwa 2

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
-import { handleLogin, handleLogout } from './src/auth.js';
-import { initializeEditorPage, fetchAndDisplayBlogPosts, fetchAndDisplaySinglePost, initializeCarousel } from './src/blog-posts.js';
-import { handleMobileMenu } from './src/ui.js';
+import { handleLogin, handleLogout } from "./src/auth.js"
+import {
+  initializeEditorPage,
+  fetchAndDisplayBlogPosts,
+  fetchAndDisplaySinglePost,
+  initializeCarousel,
+} from "./src/blog-posts.js"
+import { handleMobileMenu } from "./src/ui.js"
 
-document.addEventListener('DOMContentLoaded', () => {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then(
-        (registration) => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        },
-        (err) => {
-          console.log('ServiceWorker registration failed: ', err);
-        }
-      );
-    });
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPath = window.location.pathname
 
-  const currentPath = window.location.pathname;
-
-  if (currentPath.includes('/admin.html')) {
-    document.getElementById('login-form')?.addEventListener('submit', handleLogin);
-  } else if (currentPath.includes('/admin/editor.html')) {
-    initializeEditorPage();
-    document.getElementById('logout-button')?.addEventListener('click', handleLogout);
-  } else if (currentPath.includes('/blog.html')) {
+  if (currentPath.includes("/admin.html")) {
+    document
+      .getElementById("login-form")
+      ?.addEventListener("submit", handleLogin)
+  } else if (currentPath.includes("/admin/editor.html")) {
+    initializeEditorPage()
+    document
+      .getElementById("logout-button")
+      ?.addEventListener("click", handleLogout)
+  } else if (currentPath.includes("/blog.html")) {
     AOS.init({
       offset: 120,
       duration: 600,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-    });
-    fetchAndDisplayBlogPosts();
-  } else if (currentPath.includes('/post.html')) {
-    fetchAndDisplaySinglePost();
-  } else if (currentPath === '/' || currentPath.includes('/index.html')) {
-    initializeCarousel();
+    })
+    fetchAndDisplayBlogPosts()
+  } else if (currentPath.includes("/post.html")) {
+    fetchAndDisplaySinglePost()
+  } else if (currentPath === "/" || currentPath.includes("/index.html")) {
+    initializeCarousel()
   }
 
-  handleMobileMenu();
-});
+  handleMobileMenu()
+})
